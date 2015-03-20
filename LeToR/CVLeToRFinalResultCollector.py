94e02463b4837d09724b2e1fd9e30ae5fea87675
chenyanxiong <xiongchenyan@gmail.com>
2015-03-19 21:41:11 -0400

    init two final components

---
+++ LeToR/CVLeToRFinalResultCollector.py
@@ -0,0 +1,14 @@
+'''
+Created on Mar 19, 2015 9:39:49 PM
+@author: cx
+
+what I do:
+I collect the final results get by
+CVTrainJobSubmitter
+CVTestJobSubmitter
+what's my input:
+the predict_%d stuff in the work dir   (trec format)
+qrel
+what's my output:
+the evaluation format I used as always
+'''
