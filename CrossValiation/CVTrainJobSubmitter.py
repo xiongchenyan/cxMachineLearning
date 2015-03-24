4e6238ccf99052247f2136c1df6b3c316ddfed29
chenyanxiong <xiongchenyan@gmail.com>
2015-03-24 16:33:06 -0400

    log level

--- CrossValiation/CVTrainJobSubmitter.py
+++ CrossValiation/CVTrainJobSubmitter.py
@@ -130,7 +130,7 @@ class CVTrainJobSubmitterC(cxBaseC):
 if __name__ == '__main__':
     
     import sys
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     if 2 != len(sys.argv):
         CVTrainJobSubmitterC.ShowConf()
         sys.exit()
